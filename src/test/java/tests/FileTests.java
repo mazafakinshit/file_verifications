ae672eda0fbed01e203a75bec1ceeb2a1ea5fab5
mazafakinshit <mazafakinshit@ya.ru>
2020-06-16 20:52:46 +0300

    added speps

--- src/test/java/tests/FileTests.java
+++ src/test/java/tests/FileTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import utils.FileUtils;
 
+import static io.qameta.allure.Allure.step;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -20,10 +21,15 @@ class FileTests {
     @Test
     void successfulFileCheck() {
         String expectedFileText = "Hello ugly world!";
-        String actualFileText = new FileUtils().readStringFromFile("src/test/resources/files/file.txt");
-        System.out.println("Actual text from file: \n" + actualFileText);
+        final String[] actualFileText = {""};
 
-        assertThat(actualFileText, containsString(expectedFileText));  // схожий метод assertTrue(actualFileText.contains(expectedFileText),"Expected text: \n" + expectedFileText + "\n" + "Actual text: \n" + actualFileText);
+        step("read file", ()-> {
+            actualFileText[0] = new FileUtils().readStringFromFile("src/test/resources/files/file.txt");
+            System.out.println("Actual text from file: \n" + actualFileText[0]);
+        });
 
+        step("assert content in file", ()-> {
+            assertThat(actualFileText[0], containsString(expectedFileText));  // схожий метод assertTrue(actualFileText.contains(expectedFileText),"Expected text: \n" + expectedFileText + "\n" + "Actual text: \n" + actualFileText);
+        });
     }
 }
