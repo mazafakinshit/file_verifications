91b1e1a3be729050826759e2d07b191f66dff219
thascalos <dolzhikds@gmail.com>
2020-06-16 09:51:49 +0300

    Added Allure listener and steps

--- src/test/java/tests/ZipFileTests.java
+++ src/test/java/tests/ZipFileTests.java
@@ -2,12 +2,14 @@ package tests;
 
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import utils.FileUtils;
 import utils.ZipUtils;
 
 import static com.codeborne.selenide.Selenide.sleep;
+import static io.qameta.allure.Allure.step;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -15,11 +17,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 @Epic("QA.GURU automation course")
 @Story("Zip File tests")
 @Tag("Zip")
-@Tag("File")
-class ZipFileTests {
-
+class ZipFileTests extends TestBase {
 
     @Test
+    @DisplayName("Проверка наличия текста в файле из архива")
     void successfulZipFileCheck() {
         String source = "src/test/resources/files/files.zip";
         String destination = "src/test/resources/files/unzip";
@@ -32,11 +33,11 @@ class ZipFileTests {
         String actualFileText = new FileUtils().readStringFromFile("src/test/resources/files/unzip/file4.txt");
         System.out.println("Actual text from file: \n" + actualFileText);
 
-        assertThat(actualFileText, containsString(expectedFileText));
+        step("Проверяем наличие текста", () -> assertThat(actualFileText, containsString(expectedFileText)));
     }
 
-
     @Test
+    @DisplayName("Проверка наличия текста в файле из архива с паролем")
     void successfulZipFileWithPasswordCheck() {
         String source = "src/test/resources/files/files_wp.zip";
         String destination = "src/test/resources/files/unzip_wp";
@@ -44,12 +45,14 @@ class ZipFileTests {
 
         String expectedFileText = "Hello ugly world!";
 
-        new ZipUtils().unzip(source, destination, password);
-        sleep(10000);
+        step("Распаковываем архив с паролем", () -> {
+            new ZipUtils().unzip(source, destination, password);
+            sleep(10000);
+        });
 
         String actualFileText = new FileUtils().readStringFromFile("src/test/resources/files/unzip_wp/file4.txt");
         System.out.println("Actual text from file: \n" + actualFileText);
 
-        assertThat(actualFileText, containsString(expectedFileText));
+        step("Проверяем наличие текста", () -> assertThat(actualFileText, containsString(expectedFileText)));
     }
 }
